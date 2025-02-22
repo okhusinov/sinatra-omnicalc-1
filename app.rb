cd13709901eb1815981eff69088647d48e840da8
okhusinov <okhusinov@chicagobooth.edu>
2025-02-22 02:52:10 +0000

    mid

--- app.rb
+++ app.rb
@@ -2,19 +2,36 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  erb(:homepage)
+  erb(:square_new)
 end
 
 get("/square/new") do
-  @num_dice = params.fetch("dice").to_i
+  erb(:square_new)
+end
 
-  @num_sides = params.fetch("sides").to_i
+get("/square/results") do
+  @num_square_input = params.fetch("number").to_f
+  @num_square_result = @num_square_input ** 2
+  erb(:square_results)
+end
 
-  @rolls = []
+get("/square_root/new") do
+  erb(:square_root_new)
+end
 
-  @num_dice.times do
-    @rolls.push(rand(1..@num_sides))
-  end
-  
-  erb(:results)
+get("/square_root/results") do
+  @num_square_root_input = params.fetch("user_number").to_f
+  @num_square_root_result = @num_square_root_input ** 0.5
+  erb(:square_root_results)
+end
+
+get("/random/new") do
+  erb(:random_new)
+end
+
+get("/random/results") do
+  @num_user_min_input = params.fetch("user_min").to_f
+  @num_user_max_input = params.fetch("user_max").to_f
+  @num_random_result = rand(@num_user_min_input..@num_user_max_input)
+  erb(:random_results)
 end
